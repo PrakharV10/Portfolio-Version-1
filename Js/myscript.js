0a2c9262dbc179e2518b12f89d8cd4b0b9d4f949
PrakharV10 <70899048+PrakharV10@users.noreply.github.com>
2020-11-30 21:01:50 +0530

    Fixed : Margin bug

--- Js/myscript.js
+++ Js/myscript.js
@@ -1,13 +1,13 @@
 var menu = document.getElementById('menu');
-        var nav = document.getElementById('nav');
-        var exit = document.getElementById('exit');
+var nav = document.getElementById('nav');
+var exit = document.getElementById('exit');
 
-        menu.addEventListener('click',function(e){
-            nav.classList.toggle('hide-mobile');
-            e.preventDefault();
-        })
+menu.addEventListener('click',function(e){
+    nav.classList.toggle('hide-mobile');
+    e.preventDefault();
+})
 
-        exit.addEventListener('click',function(e){
-            nav.classList.add('hide-mobile');
-            e.preventDefault();
-        })  
+exit.addEventListener('click',function(e){
+    nav.classList.add('hide-mobile');
+    e.preventDefault();
+})  
